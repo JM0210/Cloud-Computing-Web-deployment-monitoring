88503dfe7ad45b41e9152f74520104137198543c
Junjie Mai <jamiemai0210@gmail.com>
2026-03-21 18:55:08 -0400

    Deploy TODO in container

--- Web/app.py
+++ Web/app.py
@@ -122,7 +122,8 @@ def about():
 if __name__ == "__main__":
 	env = os.environ.get('FLASK_ENV', 'development')
 	port = int(os.environ.get('PORT', 5000))
+	host = os.environ.get('HOST')
 	debug = False if env == 'production' else True
-	app.run(debug=True)
-	app.run(port=port, debug=debug)
+	# app.run(debug=True)
+	app.run(host=host, port=port, debug=debug)
 	# Careful with the debug mode..
